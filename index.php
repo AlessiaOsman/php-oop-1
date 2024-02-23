a4e7044252807d338c4c19ce5e39c32cc1f010ed
AlessiaOsman <alessia.osman@gmail.com>
2024-02-23 17:49:29 +0100

    Bonus 1

--- index.php
+++ index.php
@@ -1,35 +1,52 @@
 <?php
-class Movie 
+class Movie
 {
     public $name;
     public $year;
     public $category;
+    public $director;
 
-    public function __construct($name, $year, $category )
+    public function __construct($name, $year, $category, Director $director)
     {
         $this->name = $name;
         $this->year = $year;
         $this->category = $category;
+        $this->director = $director;
     }
 
     public function sentence()
     {
-        echo $this->name . ' è del ' . $this->year . ' ed è sotto la categoria ' . $this->category;
+        return $this->name . ' è del ' . $this->year . ' ed è sotto la categoria ' . $this->category . ' (diretto da ' .$this->director->fullName() .')';
     }
 };
 
-$first_movie = new Movie('Inception', '2010', 'Fantascienza' );
+class Director
+{
+    public $first_name;
+    public $last_name;
+
+    public function __construct($first_name, $last_name)
+    {
+        $this->first_name = $first_name;
+        $this->last_name = $last_name;
+    }
+
+    public function fullName()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
+};
+
+
+$first_movie = new Movie('Inception', '2010', 'Fantascienza', new Director('Christopher', 'Nolan'));
 var_dump($first_movie);
 
-$second_movie = new Movie('La La Land', '2016', 'Musical' );
+$second_movie = new Movie('La La Land', '2016', 'Musical', new Director('Damien', 'Chazelle'));
 var_dump($second_movie);
 
-$third_movie = new Movie('Il Padrino', '1972', 'Drammatico' );
+$third_movie = new Movie('Il Padrino', '1972', 'Drammatico', new Director('Francis', 'Ford Coppola'));
 var_dump($third_movie);
 
-$first_movie->sentence();
-$second_movie->sentence();
-$third_movie->sentence();
-
-
-?>
+echo $first_movie->sentence();
+echo $second_movie->sentence();
+echo $third_movie->sentence();
