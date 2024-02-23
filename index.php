624ad5ea96b845fa01eb2aa8afa99021546b443b
AlessiaOsman <alessia.osman@gmail.com>
2024-02-23 16:20:52 +0100

    added construct

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ class Movie
     public $year;
     public $category;
 
-    
+    function __construct($name, $year, $category )
+    {
+        $this->name = $name;
+        $this->year = $year;
+        $this->category = $category;
+    }
 }
 ?>
